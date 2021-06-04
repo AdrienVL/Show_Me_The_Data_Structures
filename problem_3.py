7d75e89db3d070ce49a73d52301f04d0371b4de2
AdrienVL <adrienlhemann@gmail.com>
2021-06-04 14:00:37 -0500

    Creating Trees

--- problem_3.py
+++ problem_3.py
@@ -1,41 +1,13 @@
 import sys
 from heapq import heapify, heappush, heappop
 
-# class childNode:
-#     def __init__(self, character, frequency):
-#         self.character = character
-#         self.frequency = frequency
-
-        
-# class internalNode:
-#     def __init__(self, frequency):
-#         self.frequency = frequency
-#         self.left = None
-#         self.right = None
-
-#     def set_left_child(self,left):
-#         self.left = left
-        
-#     def set_right_child(self, right):
-#         self.right = right
-        
-#     def get_left_child(self):
-#         return self.left
-
-#     def get_right_child(self):
-#         return self.right
-
-    # def set_frequency(self,frequency):
-    #     self.frequency = frequency
-        
-    # def get_frequency(self):
-    #     return self.frequency
 
 class Node(object):
         
     def __init__(self, frequency = None, character = None):
         self.character = character
         self.frequency = frequency
+        self.huffmanCode = None
         self.left = None
         self.right = None
         self.parent = None
@@ -45,6 +17,12 @@ class Node(object):
     
     def get_character(self):
         return self.character
+
+    def set_huffmanCode(self,huffmanCode):
+        self.huffmanCode = huffmanCode
+
+    def get_huffmanCode(self):
+        return self.huffmanCode
         
     def set_left_child(self,left):
         self.left = left
@@ -77,6 +55,36 @@ class Node(object):
     def __str__(self):
         return f"Node({self.get_frequency()})"
 
+    def __lt__(self, other):
+            return self.get_frequency() < other.get_frequency()
+
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+        
+    def get_root(self):
+        return self.root
+
+def pre_order(tree):
+    
+    visit_order = list()
+    
+    def traverse(node):
+        if node:
+            # visit the node
+            visit_order.append(node.get_frequency())
+            
+            # traverse left subtree
+            traverse(node.get_left_child())
+            
+            # traverse right subtree
+            traverse(node.get_right_child())
+
+    # traverse(tree.get_root())
+    traverse(tree)
+    
+    return visit_order
+
 def huffman_encoding(data):
 
     #1. Determine frequency of each character in message
@@ -108,8 +116,10 @@ def huffman_encoding(data):
 
     
     for key in stringDictionary:
+        #Key is character
+        #stringDic[key] is frequency
         node = Node(stringDictionary[key],key)
-        heappush(heap,(node.get_frequency(),node.get_character()))
+        heappush(heap,(node.get_frequency(),node))
 
 
     for i in heap:
@@ -118,39 +128,44 @@ def huffman_encoding(data):
     print('+++++++++++++')
 
 
-
-
-    node = None    
+ 
 
     while len(heap) != 1:
+
+
+
         firstPop = heappop(heap)
         secondPop = heappop(heap)
+    
 
-        # print(firstPop[1])
-
-        if node is None:
-            node = Node(firstPop[0]+secondPop[0])
-        else:
-            node.set_parent(Node(firstPop[0]+secondPop[0]))
-            node = node.get_parent() 
-
-        print(node)
-        if firstPop[0] <= secondPop[0]:
-            node.set_left_child(Node(firstPop[0],firstPop[1]))
-            node.set_right_child(Node(secondPop[0],secondPop[1]))
-        else:
-            node.set_right_child(Node(firstPop[0],firstPop[1]))
-            node.set_left_child(Node(secondPop[0],secondPop[1]))
-
-        heappush(heap,(node.get_frequency(),node.get_character()))
+        parent = Node(firstPop[0] + secondPop[0])
+        parent.set_left_child(firstPop[1])
+        parent.set_right_child(secondPop[1])
 
 
 
-    # print(node.get_left_child())
+
+        heappush(heap,(parent.get_frequency(),parent))
+
+       
+
+    # print(node.get_freq uency())
+    # tree = Tree(node.get_frequency())
+
+    #Recursively Remove nodes using DFS method.
+    encoded_data = ""
+
+    # print(tree.get_root())
+
+
+    # print(parent.get_left_child().get_left_child().get_left_child())
+
+    # print(pre_order(parent))
 
 
 
-    return heap[0]
+
+    # return heap[0]
 
 
 
