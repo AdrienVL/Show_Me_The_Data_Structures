d28a62a4f0b2f6261356c96c8da01bba5af9af2e
AdrienVL <adrienlhemann@gmail.com>
2021-06-28 12:14:43 -0500

    Building Test Cases

--- problem_6.py
+++ problem_6.py
@@ -16,7 +16,10 @@ class LinkedList:
         cur_head = self.head
         out_string = ""
         while cur_head:
-            out_string += str(cur_head.value) + " -> "
+            if cur_head.next:
+                out_string += str(cur_head.value) + " -> "
+            else:
+                out_string += str(cur_head.value)
             cur_head = cur_head.next
         return out_string
 
@@ -43,16 +46,13 @@ class LinkedList:
         return size
 
     #Append node values to list
-
     def to_list(self):
-        
         toList = []
         
         node = self.head 
         while node:
             toList.append(node.value)
             node = node.next
-
         
         return toList
 
@@ -63,6 +63,8 @@ def union(list_1, list_2):
 
     py_list = list_1.to_list()
     py_list2 = list_2.to_list()
+
+    #Set Removes Duplicates
     py_f_list = set(py_list + py_list2)
 
     for i in py_f_list:
@@ -75,15 +77,17 @@ def intersection(llist_1, llist_2):
 
     intersection_list = LinkedList()
 
-    py_list = llist_1.to_list()
-    py_list2 = llist_2.to_list()
+    #Set removes duplicates
+    py_list = set(llist_1.to_list())
+    py_list2 = set(llist_2.to_list())
 
+    #Keep elements from both lists
+    py_f_list=(set(py_list) & set(py_list2))
 
-    for i in py_list:
+    for i in py_f_list:
         intersection_list.append(i)
 
     return intersection_list
-
 
 
 # Test case 1
@@ -100,22 +104,33 @@ for i in element_1:
 for i in element_2:
     linked_list_2.append(i)
 
+print("=========Test Case 1 Lists =======")
+print(element_1)
+print(element_2)
+
+print("======== Test Case 1 Union=========")
 print (union(linked_list_1,linked_list_2))
-# print (intersection(linked_list_1,linked_list_2))
+print (intersection(linked_list_1,linked_list_2))
 
 # Test case 2
 
 linked_list_3 = LinkedList()
 linked_list_4 = LinkedList()
 
-element_1 = [3,2,4,35,6,65,6,4,3,23]
-element_2 = [1,7,8,9,11,21,1]
+element_3 = [3,2,4,35,6,65,6,4,3,23]
+element_4 = [1,7,8,9,11,21,1]
 
-for i in element_1:
+for i in element_3:
     linked_list_3.append(i)
 
-for i in element_2:
+for i in element_4:
     linked_list_4.append(i)
 
-# print (union(linked_list_3,linked_list_4))
-# print (intersection(linked_list_3,linked_list_4))
+print("=========Test Case 2 Lists =======")
+print(element_3)
+print(element_4)
+
+print("======== Test Case 2 Union=========")
+print (union(linked_list_3,linked_list_4))
+print (intersection(linked_list_3,linked_list_4))
+
